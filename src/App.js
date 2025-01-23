0a913a3cb5da426ea5063b181881d2033ec5c10e
Sahil Amberkar <sahilamberkar2003@gmail.com>
2025-01-23 11:43:11 +0530

    Day2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./FunCompo/Navbar.js";
+import Tab from "./FunCompo/Tab.js";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Tab />
+    </>
+  );
+}
+
+export default App;
